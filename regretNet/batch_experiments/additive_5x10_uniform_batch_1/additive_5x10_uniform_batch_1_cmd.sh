d8b4dbaa2033e9846772eaf4f502d4531a4b8940
Daniel Reusche <code@degregat.net>
2019-09-13 03:15:14 +0200

    comparison_dp_no_dp_additive_5x10_uniform Setting: additive_5x10_uniform Noise: 0.0001 0.001 0.01 0.1 Clip: 1.0 Iterations: 100000

---
+++ regretNet/batch_experiments/additive_5x10_uniform_batch_1/additive_5x10_uniform_batch_1_cmd.sh
@@ -0,0 +1,1 @@
+run_batch.py --setting additive_5x10_uniform --noise-vals 0.0001 0.001 0.01 0.1 --clip-vals 1.0 --iterations 100000 --add-no-dp-run --description comparison_dp_no_dp_additive_5x10_uniform
